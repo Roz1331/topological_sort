463774ee3ac35e84480643a86eced68f253e6b52
Roz1331 <45918469+Roz1331@users.noreply.github.com>
2020-10-19 20:43:14 +0300

    basic functionality

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -1,6 +1,24 @@
-require "topological_sort/version"
+# require_relative "topological_sort/version"
 
-module TopologicalSort
-  class Error < StandardError; end
-  # Your code goes here...
+class TopologicalSort
+  attr_accessor :post_order
+
+  def initialize(graph)
+    @post_order = []
+    @visited = []
+
+    graph.nodes.each do |node|
+      dfs(node) unless @visited.include?(node)
+    end
+  end
+
+  private
+  def dfs(node)
+    @visited << node
+    node.adjacents.each do |adj_node|
+      dfs(adj_node) unless @visited.include?(adj_node)
+    end
+
+    @post_order << node
+  end
 end
