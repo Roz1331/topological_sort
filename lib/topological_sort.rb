93ea82477a4b30bc0687905d5b82aa37740caa26
Roz1331 <45918469+Roz1331@users.noreply.github.com>
2020-10-25 19:56:58 +0300

    installed bundle gem and added first test

--- lib/topological_sort.rb
+++ lib/topological_sort.rb
@@ -1,24 +1,5 @@
 require_relative "topological_sort/version"
 
 module TopologicalSort
-  attr_accessor :post_order
 
-  def initialize(graph)
-    @post_order = []
-    @visited = []
-
-    graph.nodes.each do |node|
-      dfs(node) unless @visited.include?(node)
-    end
-  end
-
-  private
-  def dfs(node)
-    @visited << node
-    node.adjacents.each do |adj_node|
-      dfs(adj_node) unless @visited.include?(adj_node)
-    end
-
-    @post_order << node
-  end
 end
--- test/topological_sort_test.rb
+++ test/topological_sort_test.rb
@@ -1,4 +1,7 @@
 require "test_helper"
+require_relative "../lib/graph"
+require_relative "../lib/node"
+require_relative "../lib/TopologicalSortClass"
 
 class TopologicalSortTest < Minitest::Test
   def test_that_it_has_a_version_number
@@ -6,6 +9,21 @@ class TopologicalSortTest < Minitest::Test
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
+  end
+
+  def test_topological_sort_1
+    graph = Graph.new
+    graph.nodes << (node1 = Node.new(1))
+    graph.nodes << (node2 = Node.new(2))
+    graph.nodes << (node3 = Node.new(3))
+    graph.nodes << (node4 = Node.new(4))
+    graph.nodes << (node5 = Node.new(5))
+    graph.add_edge(node1, node2)
+    graph.add_edge(node1, node5)
+    graph.add_edge(node2, node3)
+    graph.add_edge(node2, node4)
+    res = [Node.new(3), Node.new(4), Node.new(2), Node.new(5), Node.new(1)]
+    assert TopologicalSortClass.new(graph).post_order.map(&:to_s) == res.map(&:to_s)
   end
 end
